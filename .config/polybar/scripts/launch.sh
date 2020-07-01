f14bafdc78f54a9c38cb843ee120cc655e87f24b
Jeff L <jeff.pull.push@gmail.com>
2020-06-30 20:42:22 -0400

    Added config of several applications.

---
+++ .config/polybar/scripts/launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+ 
+# Terminate already running bar instances
+killall -q polybar
+ 
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+sleep 2
+
+# Launch bar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mainbar-i3 &
+  done
+else
+  polybar --reload mainbar-i3 &
+fi
+ 
+echo "Bar launched"
